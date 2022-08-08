d431f2bc35ba93961bf402ff19c538eaaf97eea7
Lucaponz91 <lucapo1991@msn.com>
2022-08-08 21:31:43 +0200

    grid completed

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ const buttonElement = document.querySelector('button[type="submit"]');
 const gridEl = document.querySelector('.grid_container')
 generaGriglia(60, 30)
 buttonElement.addEventListener('click', function () {
-const width = parseInt(document.querySelector('input[name="larghezza"]').value);
-const height = parseInt(document.querySelector('input[name="altezza"]').value);
+let width = parseInt(document.querySelector('input[name="larghezza"]').value);
+let height = parseInt(document.querySelector('input[name="altezza"]').value);
 console.log(width, height)
 console.dir(buttonElement)
 generaGriglia(width, height)
@@ -21,7 +21,9 @@ function generaGriglia(x, y){
     gridEl.innerHTML = ''
     for (i=0; i< x*y; i++){
         const cella = creaCelleDiv()
+        cella.dataset.numero = i + 1
         gridEl.append(cella)
+
 
     }    
 }
@@ -30,10 +32,26 @@ function generaGriglia(x, y){
 // ##### FUNZIONE CREAZIONE DIV
 function creaCelleDiv () {
     // creo il div
+    let width = parseInt(document.querySelector('input[name="larghezza"]').value);
+    console.log(width)
+
     const div = document.createElement('div')
+    // div.innerHTML += `style="width: calc(100% / ${width});"`
+    if (isNaN(width)){
+        div.style.cssText += `width:calc(100% / 60)`
+        console.log('quaqua')
+
+    } else {
+
+    
+    div.style.cssText += `width:calc(100% / ${width} )`
+    }
+    // div.style.setProperty('width', 'calc(100% / ${width})');
+
     // aggiungo la classe cella ai div creati
     div.classList.add('cella')
     // Aggiungo l'event listener ad ogni div
     // div.addEventListener('click', clickHandler)
+    console.dir(div)
     return div
 }
